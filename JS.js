7db1ce2ba58e5f74dd7e84d03c093afd0b29a384
prak005 <prakharmallick005@gmail.com>
2025-08-03 03:54:18 +0530

    Fix buttons behaviour

--- JS.js
+++ JS.js
@@ -5,6 +5,32 @@ let input = +grid.value;
 const containerWidth = 650;
 
 container.setAttribute("style", "width:" + containerWidth + "px;");
+let color = "grey";
+const buttons = document.querySelectorAll(".color-btn");
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        color = btn.id;
+        buttons.forEach(b => {
+            b.style.backgroundColor = "white";
+            b.style.color = "black";
+        });
+        btn.style.backgroundColor = btn.id;
+        btn.style.color = "yellow";
+        if (btn.id == "white") {
+            btn.style.backgroundColor = "black";
+            btn.style.color = "white";
+        }
+    });
+});
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+    color = "grey";
+    buttons.forEach(b => {
+        b.style.backgroundColor = "white";
+        b.style.color = "black";
+    });
+    generateGrid(input);
+});
 
 function generateGrid(input) {
     let margin = 64 / input;
@@ -22,9 +48,9 @@ function generateGrid(input) {
             display: inline-block;
         `);
         div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "orange";
-    });
-        container.appendChild(div); 
+            div.style.backgroundColor = color;
+        });
+        container.appendChild(div);
     }
 }
 generateGrid(input);    //initial grid loaded at page reload
