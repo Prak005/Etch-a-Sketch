cc2bc06c8609e0da8cd680a86313902b58c08272
prak005 <prakharmallick005@gmail.com>
2025-08-03 02:32:50 +0530

    Fix boxes disappearing

--- JS.js
+++ JS.js
@@ -1,13 +1,13 @@
 const container = document.querySelector("#container");
 const grid = document.querySelector("input");
 
-let input = +grid.value ;           
-const containerWidth = 650;                       
+let input = +grid.value;
+const containerWidth = 650;
 
 container.setAttribute("style", "width:" + containerWidth + "px;");
 
 function generateGrid(input) {
-    let margin = 64/input;
+    let margin = 64 / input;
     const boxSize = (containerWidth / input) - margin * 2;          //dynamic boxSize 
 
     container.innerHTML = "";
@@ -19,8 +19,12 @@ function generateGrid(input) {
             width: ${boxSize}px;
             background-color: grey;
             margin: ${margin}px;
+            display: inline-block;
         `);
-        container.appendChild(div);
+        div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "orange";
+    });
+        container.appendChild(div); 
     }
 }
 generateGrid(input);    //initial grid loaded at page reload
